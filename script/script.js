9cd9a681baff2acb8620364bca270202344a3975
cuche2003 <notnguyentuyen@gmail.com>
2021-09-22 20:53:32 +0700

    Finished the basic interface and lunatic difficulty

--- script/script.js
+++ script/script.js
@@ -29,15 +29,15 @@ const Gameboard = (function () {
     ]
 
     function uniqueCountPreserve(inputArray){  //function to sort by reoccurence
-        var arrayItemCounts = {};
-        for (var i in inputArray){
+        let arrayItemCounts = {};
+        for (let i in inputArray){
             if (!(arrayItemCounts.hasOwnProperty(inputArray[i]))){
                 arrayItemCounts[inputArray[i]] = 1
             } else {
                 arrayItemCounts[inputArray[i]] += 1
             }
         }
-        var keysByCount = Object.keys(arrayItemCounts).sort(function(a, b){
+        let keysByCount = Object.keys(arrayItemCounts).sort(function(a, b){
             return arrayItemCounts[a]-arrayItemCounts[b];
         });
     
@@ -64,8 +64,8 @@ const Gameboard = (function () {
 
     const playTile = e => {
         if (player1.getState() && e.target.textContent === '') {
-            e.target.textContent = '🌕';
-            checkResult();
+            e.target.textContent = 'O';
+            checkMatchResult();
             player1.changeState();
             player2.changeState();
             if (!player1.getState() && player2.getState() && player2.isCPU()) {
@@ -73,8 +73,8 @@ const Gameboard = (function () {
             } 
 
         }   else if (!player2.isCPU() && player2.getState() && e.target.textContent === '')  {
-                    e.target.textContent = '🌞';
-                    checkResult();
+                    e.target.textContent = 'X';
+                    checkMatchResult();
                     player1.changeState();
                     player2.changeState();
             }
@@ -83,8 +83,8 @@ const Gameboard = (function () {
     const cpuPlay = () => {                   //cpu play
         let tile = cpuImp();
         setTimeout( () => {
-            _board[tile].textContent = '🌞';
-            checkResult();
+            _board[tile].textContent = 'X';
+            checkMatchResult();
             player1.changeState();
             player2.changeState();}
         ,1000);
@@ -98,58 +98,79 @@ const Gameboard = (function () {
 
     const cpuImp = () => {
         let choice;
-        let tiles = [];
+        let tiles = [0,1,2,3,4,5,6,7,8];
         
         for (let i = 0; i < _board.length; i++) {
-            tiles[i] = _board[i].textContent;
+            if (_board[i].textContent !== '') tiles[i] = _board[i].textContent;
         }
-        
-        for (let i = 0; i < winCombi.length; i++) {  //aim to win (fill the third one)
-            if (tiles[winCombi[i][2]] === '' && tiles[winCombi[i][0]] === '🌞' && tiles[winCombi[i][0]] === tiles[winCombi[i][1]])
-                {choice = winCombi[i][2]; break;}
-                else if (tiles[winCombi[i][1]] === '' && tiles[winCombi[i][2]] === '🌞' && tiles[winCombi[i][0]] === tiles[winCombi[i][2]])
-                {choice = winCombi[i][1]; break;}
-                else if (tiles[winCombi[i][0]] === '' && tiles[winCombi[i][1]] === '🌞' && tiles[winCombi[i][1]] === tiles[winCombi[i][2]])
-                {choice = winCombi[i][0]; break;}
-        }
-        if (choice === undefined) {                  //aim to not lose (prevent filling the third one)
-            for (let i = 0; i < winCombi.length; i++) {   
-                if (tiles[winCombi[i][2]] === '' && tiles[winCombi[i][0]] === '🌕' && tiles[winCombi[i][0]] === tiles[winCombi[i][1]])
-                    {choice = winCombi[i][2]; break;}
-                    else if (tiles[winCombi[i][1]] === '' && tiles[winCombi[i][2]] === '🌕' && tiles[winCombi[i][0]] === tiles[winCombi[i][2]])
-                    {choice = winCombi[i][1]; break;}
-                    else if (tiles[winCombi[i][0]] === '' && tiles[winCombi[i][1]] === '🌕' && tiles[winCombi[i][1]] === tiles[winCombi[i][2]])
-                    {choice = winCombi[i][0]; break;}
+        if (choice === undefined) {
+            function getPossMoves(board) {
+                return  board.filter(s => s != 'O' && s != 'X');
             }
-        }
-        if (choice === undefined) {                  //choose best possible move
-            let allPlrPossWinCombi = [];
-            let currPlrPossWinCombi = [];
-            for (let i = 0; i < winCombi.length; i++) {
-                    if (tiles[winCombi[i][0]] !== '🌞' && tiles[winCombi[i][1]] !== '🌞' && tiles[winCombi[i][2]] !== '🌞') 
-                        if (tiles[winCombi[i][0]] === '🌕' || tiles[winCombi[i][1]] === '🌕' || tiles[winCombi[i][2]] === '🌕') {
-                            currPlrPossWinCombi = currPlrPossWinCombi.concat(winCombi[i]);
+            function checkWin(board, player){
+                if (
+                       (board[0] == player && board[1] == player && board[2] == player) ||
+                       (board[3] == player && board[4] == player && board[5] == player) ||
+                       (board[6] == player && board[7] == player && board[8] == player) ||
+                       (board[0] == player && board[3] == player && board[6] == player) ||
+                       (board[1] == player && board[4] == player && board[7] == player) ||
+                       (board[2] == player && board[5] == player && board[8] == player) ||
+                       (board[0] == player && board[4] == player && board[8] == player) ||
+                       (board[2] == player && board[4] == player && board[6] == player)
+                       ) {
+                       return true;
+                   } else {
+                       return false;
+                   }
+               }
+            function minimax(board, player) {
+                let possMoves = getPossMoves(board);
+                let allMoves = [];
+                if (checkWin(board, 'O')) return {score: -10};
+                if (checkWin(board, 'X')) return {score: 10};
+                if (possMoves.length === 0) return {score: 0};
+                    for (let i = 0; i < possMoves.length; i++) {
+                        let currMove = {};
+                            currMove.index = board[possMoves[i]];
+                        board[possMoves[i]] = player;
+                        if (player === 'O') {
+                            
+                            let result = minimax(board, 'X');
+                            currMove.score = result.score; 
                         }
-                            else allPlrPossWinCombi = allPlrPossWinCombi.concat(winCombi[i]);                        
-            }
-
-            let bestMoves = uniqueCountPreserve(currPlrPossWinCombi);
-            let allBestMoves = uniqueCountPreserve(allPlrPossWinCombi);
-            if (allBestMoves.length !== 0) {
-                for (let i = allBestMoves.length - 1; i >= 0; i--) {
-                    if (tiles[allBestMoves[i]] === '') {
-                        if (bestMoves.includes(allBestMoves[i])) {
-                            choice = allBestMoves[i]; 
-                            break
+                            else if (player === 'X') {
+                                let result = minimax(board, 'O');
+                                currMove.score = result.score; 
                         }
+                        board[possMoves[i]] = currMove.index;
+                        allMoves.push(currMove);
                     }
-                }
-            }   else {for (let i = 0; i < tiles.length; i++) {if (tiles[i] === '') {choice = i; break;}}}
-        }
-        return choice;
-    }
+                let bestMove;
+                if(player === 'X'){
+                    let bestScore = -10000;
+                    for(let i = 0; i < allMoves.length; i++){
+                        if(allMoves[i].score > bestScore){
+                            bestScore = allMoves[i].score;
+                            bestMove = i;
+                        }
+                      }
+                    }else{
+                      let bestScore = 10000;
+                      for(let i = 0; i < allMoves.length; i++){
+                        if(allMoves[i].score < bestScore){
+                          bestScore = allMoves[i].score;
+                          bestMove = i;
+                        }
+                      }
+                    }
+                    return allMoves[bestMove];
+            }
+            choice = minimax(tiles, 'X');
+            }
+        return choice.index;      
+}
 
-    const checkResult = () => {
+    const checkMatchResult = () => {
         let tiles = [];
         for (let i = 0; i < _board.length; i++) {
             tiles[i] = _board[i].textContent;
@@ -158,12 +179,12 @@ const Gameboard = (function () {
             if (tiles[winCombi[i][0]] !== '' &&
                 tiles[winCombi[i][0]] === tiles[winCombi[i][1]] &&
                 tiles[winCombi[i][0]] === tiles[winCombi[i][2]]) {
-            tiles[winCombi[i][0]] == '🌕' ? alert('🌕 wins!') : alert('🌞 wins!');
+            tiles[winCombi[i][0]] == 'O' ? alert('🌕 wins!') : alert('🌞 wins!');
             resetBoard();
             return;
         }
         }
-        if (tiles.join('').length === 18) {
+        if (tiles.join('').length === 9) {
         alert('Tie!');
         resetBoard();
     }
